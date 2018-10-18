57ef5a09e46b7bb3ac45d5717d3885f1b42bab13
lyt <215896530@qq.com>
2018-10-18 17:03:30 +0800

    Initial commit

---
+++ src/main/java/com/common/sms/CommonSmsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.common.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSmsServiceApplication.class, args);
+	}
+}
